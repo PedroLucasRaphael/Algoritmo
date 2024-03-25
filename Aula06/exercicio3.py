eaceb203515fc0a47a80c7e88b4d0d0a29dd62a6
PedroLucasRaphael <161397498+PedroLucasRaphael@users.noreply.github.com>
2024-03-25 20:19:20 -0300

    exercicios aula 6

--- Aula06/exercicio3.py
+++ Aula06/exercicio3.py
@@ -1,3 +1,4 @@
 largura = float(input('insira a largura do aposento: '))
 comprimento = float(input('insira o comprimento do aposento: '))
 altura = 2.80
+# não faço a menor ideia de como concluir este exercicio
