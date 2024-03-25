306d50b4f0b0a51fd4f6c7959240a387be82fd7d
PedroLucasRaphael <161397498+PedroLucasRaphael@users.noreply.github.com>
2024-03-25 20:15:45 -0300

    exercicios aula 6

--- Aula06/exercicio1.py
+++ Aula06/exercicio1.py
@@ -1,6 +1,6 @@
 numero= int(input('digite o número: '))
 
-if numero % 5 != 0 and numero % 3 != 0:
+if numero / 5 != 0 and numero / 3 != 0:
     print('este número é divisível por 5 e 3 ao mesmo tempo :)')
 else:
     print('este número não é divisível por 5 ou por 3 :(')
--- Aula06/exercicio3.py
+++ Aula06/exercicio3.py
@@ -1,3 +1,3 @@
 largura = float(input('insira a largura do aposento: '))
 comprimento = float(input('insira o comprimento do aposento: '))
-altura = 2.800
+altura = 2.80
